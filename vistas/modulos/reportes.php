51d752fbc15693c90c7a053cd17d4a4e626c4170
Cristian Restrepo <restrepo5088@gmail.com>
2025-06-10 16:18:06 -0500

    fix : se esta trabajando en reportes en angular

--- vistas/modulos/reportes.php
+++ vistas/modulos/reportes.php
@@ -1,4 +1,4 @@
-  <!-- Content Wrapper. Contains page content -->
+    <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
@@ -16,14 +16,74 @@
       </div><!-- /.container-fluid -->
     </section>
 
-    <!-- Main content -->
-    <section class="content">
 
-      <!-- Default box -->
-
-      <!-- /.card -->
-
+  <!-- Main content -->
+  <section class="content">
+    <div class="row">
+      <!-- Sagrado Card -->
+      <div class="col-lg-3 col-6">
+        <div class="small-box bg-info">
+          <div class="inner">
+            <h3>Sagrado</h3>
+            <p>Ver gráfica</p>
+          </div>
+          <div class="icon">
+            <i class="fas fa-chart-bar"></i>
+          </div>
+          <a href="" class="small-box-footer">
+            Ir <i class="fas fa-arrow-circle-right"></i>
+          </a>
+        </div>
+      </div>
+      <!-- Bicentenario Card -->
+      <div class="col-lg-3 col-6">
+        <div class="small-box bg-success">
+          <div class="inner">
+            <h3>Bicentenario</h3>
+            <p>Ver gráfica</p>
+          </div>
+          <div class="icon">
+            <i class="fas fa-chart-line"></i>
+          </div>
+          <a href="" class="small-box-footer">
+            Ir <i class="fas fa-arrow-circle-right"></i>
+          </a>
+        </div>
+      </div>
+      <!-- Clem Card -->
+      <div class="col-lg-3 col-6">
+        <div class="small-box bg-warning">
+          <div class="inner">
+            <h3>Clem</h3>
+            <p>Ver gráfica</p>
+          </div>
+          <div class="icon">
+            <i class="fas fa-chart-pie"></i>
+          </div>
+          <a href="" class="small-box-footer">
+            Ir <i class="fas fa-arrow-circle-right"></i>
+          </a>
+        </div>
+      </div>
+      <!-- Salesiano Card -->
+      <div class="col-lg-3 col-6">
+        <div class="small-box bg-danger">
+          <div class="inner">
+            <h3>Salesiano</h3>
+            <p>Ver gráfica</p>
+          </div>
+          <div class="icon">
+            <i class="fas fa-chart-area"></i>
+          </div>
+          <a href="" class="small-box-footer">
+            Ir <i class="fas fa-arrow-circle-right"></i>
+          </a>
+           </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
     <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+</div>
+<!-- /.content-wrapper -->
